e117c91c451121f7554a396773ccd94b09ea3075
sonlv <vanson1904@gmail.com>
2018-08-07 21:55:31 +0700

    add example

---
+++ src/main/java/com/example/demo/controller/Name.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class Name {
+	private String name;
+	private String tuoi;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTuoi() {
+		return tuoi;
+	}
+	public void setTuoi(String tuoi) {
+		this.tuoi = tuoi;
+	}	
+}
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,53 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
-	@GetMapping("/test")
-	public ResponseEntity<String> testApi() {
-		return new ResponseEntity<String>("Test OK",HttpStatus.OK);
+	@GetMapping("/test3")
+	public ResponseEntity<String> getApi3() {
+	String cd = new String("abcsedfdfd");
+		return new ResponseEntity<>(cd,HttpStatus.OK);
+	}
+	
+	@GetMapping("/test1")
+	public ResponseEntity<Map<String,String>> getApi() {
+		Map<String,String> a = new HashMap<String,String>();
+		a.put("ja", "nihon");
+		a.put("en", "eng");
+		return new ResponseEntity<>(a,HttpStatus.OK);
+	}
+	
+	@GetMapping("/test2")
+	public ResponseEntity<List<Name>> getApi2() {
+		List<Name> listName = new ArrayList<Name>();
+		Name b = new Name();
+		b.setName("Tuyen");
+		b.setTuoi("28");
+		listName.add(b);
+		Name c = new Name();
+		c.setName("Son");
+		c.setTuoi("28");
+		listName.add(c);
+		return new ResponseEntity<>(listName,HttpStatus.OK);
+	}
+	
+	@PostMapping("/insert")
+	public ResponseEntity<Name> insApi2(@RequestBody Name name) {
+		System.out.println(name.getName());
+		name.setName("new");
+		name.setTuoi("30");
+		return new ResponseEntity<>(name,HttpStatus.OK);
 	}
 }
