e1af18e0b7f8647e8f87927fd03fad6e09d8b420
sonlv <vanson1904@gmail.com>
2018-08-07 17:48:54 +0700

    up source test API

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> testApi() {
+		return new ResponseEntity<String>("Test OK",HttpStatus.OK);
+	}
+}
